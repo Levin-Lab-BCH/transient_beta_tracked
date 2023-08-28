08b790aed8df291b07a575000deea290444ea04a
ybraverman <39894450+ybraverman@users.noreply.github.com>
2023-08-28 13:00:27 -0400

    Update load_paths.m
    added defaults instead of call to external script

--- 2023-07-03_Toolbox_GP/load_paths.m
+++ 2023-07-03_Toolbox_GP/load_paths.m
@@ -1,4 +1,6 @@
 function path = load_paths()
     % helper paths for below
-    path = load_paths_gp_abcct;
+    path.beapp_files = ; %path to your beapp processed files
+    path.beapp_tag   = ''; %tag for beapp run
+    path.out_tables ; %path to save output tables (ex: fullfile(path.beapp_files,'Out_Tables'))
 end
